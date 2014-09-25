428bd67227c126282ec60e3ef99e1d9aed87456b
Jack Peterson <jack@tinybike.net>
2014-09-25 14:58:35 -0700

    added six to reqs

--- pyconsensus/__init__.py
+++ pyconsensus/__init__.py
@@ -43,7 +43,7 @@ from numpy import *
 from numpy.linalg import *
 
 __title__      = "pyconsensus"
-__version__    = "0.1.5"
+__version__    = "0.1.6"
 __author__     = "Paul Sztorc and Jack Peterson"
 __license__    = "GPL"
 __maintainer__ = "Jack Peterson"
