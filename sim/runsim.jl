41e74da9e0b9f9858e73f39d6b59460824805dc9
Jack Peterson <jack@tinybike.net>
2015-02-13 21:22:32 -0800

    swap grid rows, columns

--- sim/runsim.jl
+++ sim/runsim.jl
@@ -1,6 +1,7 @@
 using PyCall
 using DataFrames
 using Dates
+using Debug
 using HDF5, JLD
 
 include("simulate.jl")
@@ -17,46 +18,56 @@ variance_threshold_range = 0.5:0.05:0.95
 # Sensitivity analysis #
 ########################
 
-algo = "fixed_threshold"
+@debug function sensitivity(liar_threshold_range,
+                            variance_threshold_range,
+                            algo="fixed_threshold")
 
-gridrows = length(liar_threshold_range)
-gridcols = length(variance_threshold_range)
-ref_vtrue_median = zeros(gridcols, gridrows)
-exp_vtrue_median = zeros(gridcols, gridrows)
-ref_beats_median = zeros(gridcols, gridrows)
-exp_beats_median = zeros(gridcols, gridrows)
-ref_correct_median = zeros(gridcols, gridrows)
-exp_correct_median = zeros(gridcols, gridrows)
-difference_median = zeros(gridcols, gridrows)
-ref_vtrue_std = zeros(gridcols, gridrows)
-exp_vtrue_std = zeros(gridcols, gridrows)
-ref_beats_std = zeros(gridcols, gridrows)
-exp_beats_std = zeros(gridcols, gridrows)
-ref_correct_std = zeros(gridcols, gridrows)
-exp_correct_std = zeros(gridcols, gridrows)
-difference_std = zeros(gridcols, gridrows)
+    gridrows = length(liar_threshold_range)
+    gridcols = length(variance_threshold_range)
+    ref_vtrue_median = zeros(gridrows, gridcols)
+    exp_vtrue_median = zeros(gridrows, gridcols)
+    ref_beats_median = zeros(gridrows, gridcols)
+    exp_beats_median = zeros(gridrows, gridcols)
+    ref_correct_median = zeros(gridrows, gridcols)
+    exp_correct_median = zeros(gridrows, gridcols)
+    difference_median = zeros(gridrows, gridcols)
+    ref_vtrue_std = zeros(gridrows, gridcols)
+    exp_vtrue_std = zeros(gridrows, gridcols)
+    ref_beats_std = zeros(gridrows, gridcols)
+    exp_beats_std = zeros(gridrows, gridcols)
+    ref_correct_std = zeros(gridrows, gridcols)
+    exp_correct_std = zeros(gridrows, gridcols)
+    difference_std = zeros(gridrows, gridcols)
 
-for (row, liar_threshold) in enumerate(liar_threshold_range)
-    println("liar_threshold: ", liar_threshold)
-    for (col, variance_threshold) in enumerate(variance_threshold_range)
-        println("  variance_threshold: ", variance_threshold)
-        medians, stds = simulate(algo, collude, liar_threshold, variance_threshold)
-        ref_vtrue_median[row,col] = medians[1]
-        ref_beats_median[row,col] = medians[2]
-        exp_vtrue_median[row,col] = medians[3]
-        exp_beats_median[row,col] = medians[4]
-        difference_median[row,col] = medians[5]
-        ref_correct_median[row,col] = medians[6]
-        exp_correct_median[row,col] = medians[7]
-        ref_vtrue_std[row,col] = stds[1]
-        ref_beats_std[row,col] = stds[2]
-        exp_vtrue_std[row,col] = stds[3]
-        exp_beats_std[row,col] = stds[4]
-        difference_std[row,col] = stds[5]
-        ref_correct_std[row,col] = stds[6]
-        exp_correct_std[row,col] = stds[7]
+    for (row, liar_threshold) in enumerate(liar_threshold_range)
+        println("liar_threshold: ", liar_threshold)
+        for (col, variance_threshold) in enumerate(variance_threshold_range)
+            println("  variance_threshold: ", variance_threshold)
+            medians, stds = simulate(algo, collude, liar_threshold, variance_threshold)
+            # println(row, " ", col)
+            try
+                ref_vtrue_median[row,col] = medians[1]
+                ref_beats_median[row,col] = medians[2]
+                exp_vtrue_median[row,col] = medians[3]
+                exp_beats_median[row,col] = medians[4]
+                difference_median[row,col] = medians[5]
+                ref_correct_median[row,col] = medians[6]
+                exp_correct_median[row,col] = medians[7]
+                ref_vtrue_std[row,col] = stds[1]
+                ref_beats_std[row,col] = stds[2]
+                exp_vtrue_std[row,col] = stds[3]
+                exp_beats_std[row,col] = stds[4]
+                difference_std[row,col] = stds[5]
+                ref_correct_std[row,col] = stds[6]
+                exp_correct_std[row,col] = stds[7]
+            catch
+                @bp
+            end
+        end
     end
 end
+
+sensitivity(liar_threshold_range, variance_threshold_range)
 
 #####################
 # Save data to file #
